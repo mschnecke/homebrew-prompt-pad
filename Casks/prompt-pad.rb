0e827f66b0b2558865166b9ea0a898d99d4a5627
mschnecke <mschnecke@users.noreply.github.com>
2025-12-10 15:31:30 +0000

    Update PromptPad cask to v1.1.1

--- Casks/prompt-pad.rb
+++ Casks/prompt-pad.rb
@@ -1,7 +1,7 @@
 cask "prompt-pad" do
-  version "1.0.0"
-  sha256 arm:   "PLACEHOLDER_ARM64_SHA256",
-         intel: "PLACEHOLDER_X64_SHA256"
+  version "1.1.1"
+  sha256 arm:   "3e923910cb0b7e2cc47a2b1d0c16178eab76587958d8b56d78e49128ce7c0508",
+         intel: "5dc6ad166e2ecf6cd2d53caa70dd42d2e827858114148a56c1ffe4ff747c3ec9"
 
   on_arm do
     url "https://github.com/mschnecke/prompt-pad/releases/download/v#{version}/PromptPad_#{version}_aarch64.pkg"
